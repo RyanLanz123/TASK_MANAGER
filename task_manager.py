ef1a19dc33b0845d6a0659cb6b222e24c97bf146
RyanLanz123 <ryanlanz6@gmail.com>
2024-12-09 16:29:10 +0000

    create add task function and save it to txt doc

--- task_manager.py
+++ task_manager.py
@@ -1,5 +1,5 @@
 def load_password():#Load password from .txt file
-    with open('password.text', 'r') as f:
+    with open('password.txt', 'r') as f:
         return f.read().strip()
     
 def login():#Allow user to login with password
@@ -11,3 +11,15 @@ def login():#Allow user to login with password
             break
         else:
             print("Incorrect password. Try again")
+
+def add_task():
+    task_name = input("Enter task name: ")
+    deadline = input("Enter deadline (YYYY-MM-DD): ")
+    category = input("Enter category: ")
+    status = "Pending"
+    with open('tasks.txt', 'a') as f:
+        f.write(f"{task_name}|{deadline}|{category}|{status}\n")
+    print("Tasks added succesfully!")
+
+if __name__ == "__main__":
+    add_task()
